840b1c95629be27b144e7d32a44a3ace21990d6a
Cristian Canedo <cristiancanedo@outlook.com>
2017-05-08 14:06:17 -0400

    Update Board.cs

--- Board.cs
+++ Board.cs
@@ -8,7 +8,11 @@ namespace TicTacToe
 {
     public class Board
     {
-        public char[,] gb = new char[5, 6];
+        public char[,] gb = new char[5, 6]; // Initiates 2D array of type char 
+
+        /// <summary>
+        /// Contructor initializes all items within the array to draw the board and empty it
+        /// </summary>
         public Board()
         {
             gb[0, 0] = '1';
@@ -30,17 +34,20 @@ namespace TicTacToe
             gb[4, 0] = '3';
             gb[4, 2] = '|';
             gb[4, 4] = '|';
-
-            
         }
 
         public void DisplayBoard()
         {
-            Console.Clear();
-            int rowLength = this.gb.GetLength(0);
+            Console.Clear(); // Clears the screen so that each time board is displayed, it looks as if the console
+                                // remains static
+            
+            // initiates 2 variables to hold the row and column length of the 2D array
+            int rowLength = this.gb.GetLength(0); 
             int colLength = this.gb.GetLength(1);
 
-            Console.WriteLine("  1   2   3");
+            Console.WriteLine("  1   2   3"); // The "X" coordinates
+
+            // For loop that prints the 2D array as a matrix in order to display the board
             for (int i = 0; i < rowLength; i++)
             {
                 for (int j = 0; j < colLength; j++)
@@ -49,11 +56,7 @@ namespace TicTacToe
                 }
                 Console.WriteLine();
             }
-
             Console.Write("\n");
         }
-
-
-
     }
 }
