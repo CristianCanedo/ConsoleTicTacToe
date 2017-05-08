77b743a52811259c3cdafccdb09eb16e483b6f03
Cristian Canedo <cristiancanedo@outlook.com>
2017-05-08 14:05:40 -0400

    Update Turns.cs

--- Turns.cs
+++ Turns.cs
@@ -9,10 +9,13 @@ namespace TicTacToe
     public class Turns : Board
     {
         Players p = new Players();
-        public static bool pOneWins = false;
-        public static bool pTwoWins = false;
-        public static bool isDraw = false;
+        public static bool pOneWins = false; // variable to determine whether or not player one wins
+        public static bool pTwoWins = false; // variable to determine whether or not player two wins
+        public static bool isDraw = false; // variable to determine whether or not the game is a draw
 
+
+        // Initiates the player turns and when finished checks each of the bool variables to print
+        // the corresponding message to the screen.
         public void StartTurns()
         {
             PlayerOneTurn();
@@ -20,57 +23,65 @@ namespace TicTacToe
 
             if (pOneWins)
             {
-                Console.WriteLine("Player {0} wins!\n", p.PlayerOne);
+                Console.WriteLine("Player {0} wins!", p.PlayerOne);
                 Console.ReadLine();
             }
             else if (pTwoWins)
             {
-                Console.WriteLine("Player {0} wins!\n", p.PlayerTwo);
+                Console.WriteLine("Player {0} wins!", p.PlayerTwo);
                 Console.ReadLine();
             }
             else if (isDraw)
             {
-                Console.WriteLine("Game is a draw!\n");
+                Console.WriteLine("Game is a draw!");
                 Console.ReadLine();
             }
 
 
         }
 
+
+        /// <summary>
+        /// PlayerOneTurn() & PlayerTwoTurn() only run if all bool variables are set to false.
+        /// Only then can the player enter the X and Y coordinates where he or she chooses to place a piece on the board.
+        /// </summary>
         public void PlayerOneTurn()
         {
             if (pTwoWins == false && pOneWins == false && isDraw == false)
             {
-                DisplayBoard();
                 Console.WriteLine("Player {0} turn.", this.p.PlayerOne);
                 Console.Write("Enter X Coordinate: ");
                 int x = int.Parse(Console.ReadLine());
                 Console.Write("Enter Y Coordinate: ");
                 int y = int.Parse(Console.ReadLine());
-                CheckInputOne(x, y);
+                CheckInputOne(x, y); // Calls the CheckInputOne method for PlayerOne
             }
         }
-
         public void PlayerTwoTurn()
         {
 
             if (pTwoWins == false && pOneWins == false && isDraw == false)
             {
-                base.DisplayBoard();
                 Console.WriteLine("Player {0} turn.", p.PlayerTwo);
                 Console.Write("Enter X Coordinate: ");
                 int x = int.Parse(Console.ReadLine());
                 Console.Write("Enter Y Coordinate: ");
                 int y = int.Parse(Console.ReadLine());
-                CheckInputTwo(x, y);
+                CheckInputTwo(x, y); // Calls the CheckInputTwo method for PlayerTwo
             }
         }
 
+        
+        /// <summary>
+        /// CheckInputOne() & CheckInputTwo() check the users X and Y input and place a piece
+        /// on the board if that space is empty, calls the CheckWinner() method, then passes
+        /// the turn to the next player by calling their respective player turn methods.
+        /// </summary>
         public void CheckInputOne(int x, int y)
         {
             switch (x)
             {
-                case 1:
+                case 1:// if y == 1-3, AND gb space is empty, assign 'X', if not retry
                     if (y == 1) if (base.gb[0, 1] == '\0') { base.gb[0, 1] = 'X'; base.DisplayBoard(); CheckWinner(); PlayerTwoTurn(); } else { Console.WriteLine("Cannot play that space. Press enter to choose another."); Console.ReadLine(); PlayerOneTurn(); }
                     if (y == 2) if (base.gb[2, 1] == '\0') { base.gb[2, 1] = 'X'; base.DisplayBoard(); CheckWinner(); PlayerTwoTurn(); } else { Console.WriteLine("Cannot play that space. Press enter to choose another."); Console.ReadLine(); PlayerOneTurn(); }
                     if (y == 3) if (base.gb[4, 1] == '\0') { base.gb[4, 1] = 'X'; base.DisplayBoard(); CheckWinner(); PlayerTwoTurn(); } else { Console.WriteLine("Cannot play that space. Press enter to choose another."); Console.ReadLine(); PlayerOneTurn(); }
@@ -95,7 +106,7 @@ namespace TicTacToe
         {
             switch (x)
             {
-                case 1:
+                case 1:// if y == 1-3, AND gb space is empty, assign 'O', if not retry
                     if (y == 1) if (base.gb[0, 1] == '\0') { base.gb[0, 1] = 'O'; base.DisplayBoard(); CheckWinner(); PlayerOneTurn(); } else { Console.WriteLine("Cannot play that space. Press enter to choose another."); Console.ReadLine(); PlayerTwoTurn(); }
                     if (y == 2) if (base.gb[2, 1] == '\0') { base.gb[2, 1] = 'O'; base.DisplayBoard(); CheckWinner(); PlayerOneTurn(); } else { Console.WriteLine("Cannot play that space. Press enter to choose another."); Console.ReadLine(); PlayerTwoTurn(); }
                     if (y == 3) if (base.gb[4, 1] == '\0') { base.gb[4, 1] = 'O'; base.DisplayBoard(); CheckWinner(); PlayerOneTurn(); } else { Console.WriteLine("Cannot play that space. Press enter to choose another."); Console.ReadLine(); PlayerTwoTurn(); }
@@ -116,6 +127,7 @@ namespace TicTacToe
 
         }
 
+        // CheckWinner() method steps through each Diagonal, Horizontal, and Vertical method to check the 2D array
         public void CheckWinner()
         {
             CheckDiagonal();
@@ -123,14 +135,17 @@ namespace TicTacToe
             CheckVertical();
         }
 
+        // Checks the diagonal array indexes to make sure they all equal either an 'X' or 'O' and 
+        // assigns "true" to the respective player. Else if all spaces are filled and none match,
+        // isDraw will equal true.
         private void CheckDiagonal()
         {
             if (gb[0, 1] == 'X' && gb[2, 3] == 'X' && gb[4, 5] == 'X'|| gb[4, 1] == 'X' && gb[2, 3] == 'X' && gb[0, 5] == 'X') { pOneWins = true; } // Player One Wins!
 
             else if (gb[0, 1] == 'O' && gb[2, 3] == 'O' && gb[4, 5] == 'O' || gb[4, 1] == 'O' && gb[2, 3] == 'O' && gb[0, 5] == 'O') { pTwoWins = true; } // Player Two Wins!
-            
+
             else if (gb[0,1] != '\0' && gb[2,3] != '\0' && gb[4, 5] != '\0' && gb[4, 1] != '\0' && 
-                            gb[0, 5] != '\0' && gb[0,3] != '\0' && gb[2,5] != '\0' && gb[4,3] != '\0' && gb[2, 1] != '\0') { isDraw = true; } // Game is a draw!
+                            gb[0, 5] != '\0' && gb[0,3] != '\0' && gb[2,5] != '\0' && gb[4,3] != '\0' && gb[2, 1] != '\0') { isDraw = true; } // Game is a draw! 
 
         }
 
@@ -145,7 +160,7 @@ namespace TicTacToe
                                 || gb[4, 1] == 'O' && gb[4, 3] == 'O' && gb[4, 5] == 'O') { pTwoWins = true; } // Player Two wins!
 
             else if (gb[0, 1] != '\0' && gb[0, 3] != '\0' && gb[0, 5] != '\0' && gb[2, 1] != '\0' &&
-                            gb[2, 3] != '\0' && gb[2, 5] != '\0' && gb[4, 1] != '\0' && gb[4, 3] != '\0' && gb[4, 5] != '\0') { isDraw = true; } // Game is a draw!
+                            gb[2, 3] != '\0' && gb[2, 5] != '\0' && gb[4, 1] != '\0' && gb[4, 3] != '\0' && gb[4, 5] != '\0') { isDraw = true; } // Game is a draw! 
         }
 
         private void CheckVertical()
@@ -158,8 +173,8 @@ namespace TicTacToe
                         || gb[0, 3] == 'O' && gb[2, 3] == 'O' && gb[4, 3] == 'O'
                                 || gb[0, 5] == 'O' && gb[2, 5] == 'O' && gb[4, 5] == 'O') { pTwoWins = true; } // Player Two Wins!
 
-            else if (gb[0, 1] != '\0' && gb[2, 1] != '\0' && gb[4, 1] != '\0' && gb[0, 3] != '\0' &&
-                            gb[2, 3] != '\0' && gb[4, 3] != '\0' && gb[0, 5] != '\0' && gb[2, 5] != '\0' && gb[4, 5] != '\0') { isDraw = true; } // Game is a draw!
+            else if (gb[0, 1] != '\0' && gb[0, 3] != '\0' && gb[0, 5] != '\0' && gb[2, 1] != '\0' &&
+                            gb[2, 3] != '\0' && gb[2, 5] != '\0' && gb[4, 1] != '\0' && gb[4, 3] != '\0' && gb[4, 5] != '\0') { isDraw = true; } // Game is a draw!
         }
     }
 }
